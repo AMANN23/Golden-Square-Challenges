60c802c2f79494c440c5f05af7c8ba61b5d51489
Adnan Mann <adnan_mann@outlook.com>
2023-02-20 16:18:54 +0000

    code amended

--- lib/check_codeword.rb
+++ lib/check_codeword.rb
@@ -2,7 +2,7 @@ def check_codeword(codeword)
     if codeword == "horse"
         return "Correct! Come in."
     elsif codeword.chars.first == "h" && codeword.chars.last == "e"
-        return "CLose, but nope."
+        return "Close, but nope."
     else
         return "WRONG!"
     end
